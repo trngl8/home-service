7d8cc343bd2dd28059d1e20017bd24b5e5686106
ArtemKrayevskiy <116542338+ArtemKrayevskiy@users.noreply.github.com>
2024-07-03 14:17:20 +0300

    Home page, close #1

---
+++ index.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+import os
+
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        user_name = request.form.get("name")
+        user_property_type = request.form.get("type")
+        user_square = request.form.get("square")
+        user_bathrooms = request.form.get("bathrooms")
+        user_bedrooms = request.form.get("bedrooms")
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.run(debug=os.getenv("DEVELOPMENT"))
