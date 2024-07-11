5511e94cc5b4039b105b99349b3604f3b97cdb51
ArtemKrayevskiy <kraievskyi.pn@ucu.edu.ua>
2024-07-11 16:22:53 +0300

    orders processing added

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 from flask import Flask, request, render_template
 import os
+from catalog import db
+from catalog import create_app
 
-
-
-app = Flask(__name__)
+app = create_app()
 
 @app.route('/', methods=["GET", "POST"])
 def home():
@@ -13,6 +13,13 @@ def home():
         user_square = request.form.get("square")
         user_bathrooms = request.form.get("bathrooms")
         user_bedrooms = request.form.get("bedrooms")
+        print(user_name)
+        database = db.get_db()
+        database.execute(
+            "INSERT INTO realty (name, type, square, bathrooms, bedrooms) VALUES (?, ?, ?, ?, ?)",
+            (user_name, user_property_type, user_square, user_bathrooms, user_bedrooms)
+        )
+        database.commit()
     return render_template("home.html")
 
 if __name__ == "__main__":
