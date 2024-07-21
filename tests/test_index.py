0cd4a938a6dac4b97118418baa92f11b300f2eb8
mvchn <movchan@gmail.com>
2024-07-22 01:35:59 +0300

    routes test

--- tests/test_index.py
+++ tests/test_index.py
@@ -12,13 +12,37 @@ class AppTestCase(unittest.TestCase):
         response = self.app.get('/')
         self.assertEqual(200, response.status_code)
 
+    def test_about(self):
+        response = self.app.get('/about')
+        self.assertEqual(200, response.status_code)
+
+    def test_contacts(self):
+        response = self.app.get('/contacts')
+        self.assertEqual(200, response.status_code)
+
+    def test_products(self):
+        response = self.app.get('/products')
+        self.assertEqual(200, response.status_code)
+
+    def test_services(self):
+        response = self.app.get('/services')
+        self.assertEqual(200, response.status_code)
+
+    def test_articles(self):
+        response = self.app.get('/articles')
+        self.assertEqual(200, response.status_code)
+
+    def test_order(self):
+        response = self.app.get('/order')
+        self.assertEqual(200, response.status_code)
+
     def test_order_fail(self):
-        response = self.app.post('/', data={}, follow_redirects=True)
+        response = self.app.post('/order', data={}, follow_redirects=True)
         self.assertEqual(200, response.status_code)
         self.assertIn(b'This field is required', response.data)
 
     def test_order_success(self):
-        response = self.app.post('/', data={
+        response = self.app.post('/order', data={
             "name": "Address",
             "type": "1",
             "property_type": "House",
